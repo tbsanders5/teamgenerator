98a201ea93535cafb076de8c30298d392aa0fff1
Monk <tbsanders5@gmail.com>
2020-09-11 20:39:08 -0700

    finished app functionality, app now writes and creates an html file with the added employees on the file, HTML was written, cards generated

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const userPrompt = [
         message: 'What is your email?',
         type: 'input',
         validate: function (employeeEmail) {
-            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(empEmail)
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(employeeEmail)
 
             if (valid) {
                 return true;
@@ -112,9 +112,102 @@ function createEmployee() {
         });
 }
 
+function generateHtml() {
+    let infoHTML = fs.readFileSync('./index.html', 'utf8', function(err) {
+        if (err)
+            console.log(err);
 
+    });
 
+    const $ = cheerio.load(infoHTML);
 
+    employees.forEach(user => {
+        let employeeRole = user.getRole();
+        let output;
+
+        if (employeeRole === 'Manager') {
+            output = generateManager(user.name, user.id, user.email, user.officeNum);
+        } else if (employeeRole === 'Employee') {
+            output = generateEmployee(user.name, user.id, user.email);
+        } else if (employeeRole === 'Engineer') {
+            output = generateEngineer(user.name, user.id, user.email, user.github);
+        } else if (employeeRole === 'Intern') {
+            output = generateIntern(user.name, user.id, user.email, user.school)
+        }
+
+        $('#teamGenerator').append(output);
+    });
+
+    fs.writeFile('./teamOutput.html', $.html(), function(err) {
+        if (err)
+            console.log(err);
+
+            openTeamBreakdown('./teamOutput.html')
+    });
+}
+
+async function openTeamBreakdown (fileName) {
+
+    await open(fileName, {wait:true});
+    console.log('File is opening');
+}
+
+function generateManager(name, id, email, officeNum) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Manager</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+      <li class="list-group-item">School: ${officeNum}</li>
+    </ul>
+    </div>`
+}
+
+function generateEmployee(name, id, email) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Employee</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+    </ul>
+    </div>`
+}
+
+function generateIntern(name, id, email, school) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Intern</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+      <li class="list-group-item">School: ${school}</li>
+    </ul>
+    </div>`
+}
+
+function generateEngineer(name, id, email, github) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Engineer</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+      <li class="list-group-item">School: ${github}</li>
+    </ul>
+    </div>`
+}
+
+createEmployee();
 
 
 
